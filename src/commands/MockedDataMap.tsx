85a11a24c9020ea6579790c4004edfae800b7234
Jason Estrada <jason_estrada@brown.edu>
2024-02-29 01:53:13 -0500

    feat-user-story-2 all set up and working

--- src/commands/MockedDataMap.tsx
+++ src/commands/MockedDataMap.tsx
@@ -2,20 +2,6 @@
 import { Dispatch, SetStateAction, useState} from 'react';
 export class MockedDataMap {
     private hashMap: { [key: string]: string[][] } = {};
-    private loadedFile: string[][];
-    private setLoadedFile: Dispatch<SetStateAction<(string[][])>>;
-
-    constructor(loadedFile: string[][],setLoadedFile: Dispatch<SetStateAction<(string[][])>>){
-        this.loadedFile = loadedFile;
-        this.setLoadedFile = setLoadedFile;
-    }
-    setNewLoadedFile(file: string[][]) {
-        this.setLoadedFile(file);
-    }
-
-    getLoadedFile(): string[][] {
-        return this.loadedFile;
-    }
 
     getFile(fileName: string): string[][] {
         return this.hashMap[fileName];
