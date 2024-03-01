8a98b8e21d67dfde19730288fa53cbd35a872a13
Jason Estrada <jason_estrada@brown.edu>
2024-03-01 18:53:48 -0500

    docs- commented REPL and ControlledInput

--- src/components/ControlledInput.tsx
+++ src/components/ControlledInput.tsx
@@ -1,18 +1,18 @@
 import '../styles/main.css';
 import { Dispatch, SetStateAction } from 'react';
 
-// Remember that parameter names don't necessarily need to overlap;
-// I could use different variable names in the actual function.
+// This is an interface that is passed into the function below. It ensures that these arguments are
+// passed into the function
 interface ControlledInputProps {
     value: string, 
     // This type comes from React+TypeScript. VSCode can suggest these.
-    //   Concretely, this means "a function that sets a state containing a string"
+    //Concretely, this means "a function that sets a state containing a string"
     setValue: Dispatch<SetStateAction<string>>,
     ariaLabel: string 
   }
   
   // Input boxes contain state. We want to make sure React is managing that state,
-  //   so we have a special component that wraps the input box.
+  // so we have a special component that wraps the input box.
   export function ControlledInput({value, setValue, ariaLabel}: ControlledInputProps) {
     return (
       <input type="text" className="repl-command-box"
